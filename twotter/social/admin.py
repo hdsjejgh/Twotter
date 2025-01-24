da59defff7631d4cfd4e838ace6afb05964e4a5d
hdsjejgh <aayanahmad08@hotmail.com>
2025-01-24 18:33:08 -0500

    added comments and made profiles better

--- twotter/social/admin.py
+++ twotter/social/admin.py
@@ -2,16 +2,17 @@ from django.contrib import admin
 from django.contrib.auth.models import Group,User
 from .models import Profile
 # Register your models here.
+
+#django automatically created a group model, it isnt needed
 admin.site.unregister(Group)
 
-class ProfileInline(admin.StackedInline):
+class ProfileInline(admin.StackedInline):#used to display the profile of a user in
     model = Profile
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(admin.ModelAdmin): #the actual user thing that gets displayed in admin
     model = User
     fields = ["username"]
     inlines = [ProfileInline]
 
 admin.site.unregister(User)
-admin.site.register(User, UserAdmin)
-
+admin.site.register(User, UserAdmin) #unregister and reregister the user model so it only shows the things in UserAdmin in the admin interface
--- twotter/social/models.py
+++ twotter/social/models.py
@@ -1,12 +1,24 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+
 # Create your models here.
 
-class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    follows = models.ManyToManyField("self",
+class Profile(models.Model): #profile model is in every user model
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #each user has one unique profile
+    follows = models.ManyToManyField("self", #follows contain any amount of other profiles
                                      related_name="followed_by",
-                                     symmetrical=False,
-                                     blank=True)
-    def __str__(self):
+                                     symmetrical=False, #so following doesn't have to go both ways
+                                     blank=True) #following can be blank
+    def __str__(self): #so the profile is displayed as the username
         return self.user.username
+
+@receiver(post_save,sender=User)#when user is created, call this function
+def create_profile(sender,instance,created,**kwargs):
+    if created:
+        user_profile = Profile(user=instance) #automatically creates profile when user is made
+        user_profile.save()
+        user_profile.follows.set([instance.profile.id]) #makes profil follow themselves
+        user_profile.save()
